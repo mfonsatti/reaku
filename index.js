6c059f922150e9f86d4de86a2d036084e5a8f95c
Matteo Fonsatti <matteo.fonsatti@wyscout.com>
2019-12-30 12:16:11 +0100

    fixed routes

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const path = require('path')
 // Serve static files from the React frontend app
 app.use(express.static(path.join(__dirname, 'client/build')))
 // Anything that doesn't match the above, send back index.html
+app.get('/hello', (req, res) => res.json({test:'Hello World!'}))
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/client/build/index.html'))
 })
 
-app.get('/hello', (req, res) => res.json({test:'Hello World!'}))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
